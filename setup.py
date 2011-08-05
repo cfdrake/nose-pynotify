17f5067a308430a7dadd2bd23bc7c960cc658afb
Colin Drake <colin.f.drake@gmail.com>
2011-08-04 20:17:57 -0400

    Bump the version up, since it's getting a bit more polished

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nose-pynotify",
-    version="0.1",
+    version="0.5",
     author="Colin Drake",
     author_email = "colin.f.drake@gmail.com",
     description = "Plugin for nose to use pynotify for graphical notifications",
